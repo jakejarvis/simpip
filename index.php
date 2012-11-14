27ab4ea7aebc8aeab499dfce09d2520947aaa86b
Jake Jarvis <jakejarvis@gmail.com>
2012-11-14 12:42:07 -0500

    fix HTTP_X_FORWARDED_FOR returning 127.0.0.1

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php header("Content-type: text/plain"); echo ($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR']; ?>
+<?php
+  header("Content-type: text/plain");
+  if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    if (($pos = strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ",")) !== false) {
+      echo substr($_SERVER['HTTP_X_FORWARDED_FOR'], 0, $pos);
+    } else {
+      echo $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+  } else {
+    echo $_SERVER['REMOTE_ADDR'];
+  }
+?>
