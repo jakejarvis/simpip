d33732bce14bc4cbf2275d66191c5612cc47862f
Jake Jarvis <jake@jarv.is>
2022-04-27 11:10:11 -0400

    remove git.io shortlink
    https://github.blog/changelog/2022-04-25-git-io-deprecation/

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const handle = async (request) => {
       "Cache-Control": "no-cache, no-store, must-revalidate",
       "Content-Security-Policy": "default-src 'none';",
       "X-Did-You-Know": "You can use \"curl -4\" or \"curl -6\" to get either IP address!",
-      "X-View-Source": "https://git.io/simpip",
+      "X-View-Source": "https://jrvs.io/simpip",
     },
   };
 
